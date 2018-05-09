c2821ab582149026312d3d6e4374b703dde0e525
Adeline Johnson <groblewski.adeline@gmail.com>
2018-05-08 22:39:31 -0400

    added file structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+
