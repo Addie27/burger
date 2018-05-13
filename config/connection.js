043dd7b618aef8a1dc08d20ba51f0bbf271e42db
Adeline Johnson <groblewski.adeline@gmail.com>
2018-05-13 19:57:48 -0400

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-// require("dotenv").config();
-// var keys = require("../keys.js");
+require("dotenv").config();
+var keys = require("../keys.js");
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
 
   // Your username
-  user: "root",
+  user: process.env.DB_USER,
 
   // Your password
-  password: "password",
+  password: process.env.DB_PASS,
   database: "burgers_db"
 });
 
