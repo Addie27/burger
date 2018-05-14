66295b7e805afa5b89d5ac71447a45008b690c34
Adeline Johnson <groblewski.adeline@gmail.com>
2018-05-13 21:45:47 -0400

    herokue db connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,12 @@ var mysql = require("mysql");
 require("dotenv").config();
 var keys = require("../keys.js");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
 
@@ -14,6 +19,8 @@ var connection = mysql.createConnection({
   password: process.env.DB_PASS,
   database: "burgers_db"
 });
+}; 
+
 
 
 connection.connect(function (err) {
